202bcb14a34fdb153133749ab9d15c4e0a7ab802
Pedro Pablo Fernandez Borra <pedropablofernandezborra@gmail.com>
2022-10-22 15:35:56 -0300

    Update finance.js

--- js/finance.js
+++ js/finance.js
@@ -7,24 +7,22 @@ fetch (url)
 .then(dato=>{
     console.log(dato)
     contenido.innerHTML = `
-    <a href="" class="quotes-type">
-        <p class="quotes-title">Dólar Banco Nación</p>
-        <p class="quotes-number">${dato[0].casa.venta}</p>
-    </a>
-    <a href="" class="quotes-type">
-        <p class="quotes-title">Dólar Blue</p>
-        <p class="quotes-number">${dato[1].casa.venta}</p>
-    </a>
-    <a href="" class="quotes-type">
-        <p class="quotes-title">Dólar MEP</p>
-        <p class="quotes-number">${dato[4].casa.venta}</p>
-    </a>
-    <a href="" class="quotes-type">
-        <p class="quotes-title">Dólar CCL</p>
-        <p class="quotes-number">${dato[3].casa.venta}</p>
-    </a>
-
-
+    <div class="pricing">
+        <p class="quotes-title">Dólar Banco Nación:</p>
+        <p class="quotes-number">$${dato[0].casa.venta}</p>
+    </div>
+    <div class="pricing">
+        <p class="quotes-title">Dólar Blue:</p>
+        <p class="quotes-number">$${dato[1].casa.venta}</p>
+    </div>
+    <div class="pricing">
+        <p class="quotes-title">Dólar MEP:</p>
+        <p class="quotes-number">$${dato[4].casa.venta}</p>
+    </div>
+    <div class="pricing">
+        <p class="quotes-title">Dólar CCL:</p>
+        <p class="quotes-number">$${dato[3].casa.venta}</p>
+    </div>
     `
 
 })
